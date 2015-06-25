b7c2d104be5a2245828e883e77385a9c613fabab
Kurt De Vocht <kurt.de.vocht@pandora.be>
2015-06-25 13:07:09 +0200

    Moving stuff to other folders...

---
+++ src/Wii2Scratch.ScratchHelper/IRState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wii2Scratch.ScratchHelper
+{
+    public class IRState
+    {
+        public bool Found { get; set; }
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+    }
+}
